68de4ae2da9f3f11782fb4b778c0b1b6f4f2758f
yunseohhe <dbstj4566@naver.com>
2024-08-28 01:09:57 +0900

    init commit

---
+++ src/main/java/com/sparta/calendarjpa/CalendarJpaApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.calendarjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/calendarjpa/CalendarJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.calendarjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
